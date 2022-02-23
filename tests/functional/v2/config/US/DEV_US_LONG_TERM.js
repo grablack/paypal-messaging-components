5fa3eb5ff564aae13dca18c11ac63e38f42b1fc2
Grant Black <grablack@paypal.com>
2022-02-23 16:10:05 -0500

    changed

--- tests/functional/v2/config/US/DEV_US_LONG_TERM.js
+++ tests/functional/v2/config/US/DEV_US_LONG_TERM.js
@@ -24,7 +24,7 @@ export const DEV_US_LONG_TERM = {
                 offerHeadline: '$33.17/mo. for 6 months',
                 updatedOfferHeadline: '$83.33/mo. for 6 months',
                 offerFieldValues: ['0%', '$0.00', '$199.00'],
-                aprDisclaimer: 'Terms may vary based on purchase amount.'
+                aprDisclaimer: 'Changed Terms may vary based on purchase amount.'
             }
         },
         {
